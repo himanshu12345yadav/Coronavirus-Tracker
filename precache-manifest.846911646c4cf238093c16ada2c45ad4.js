ca78916b87556894a46df7c57e3193f930894459
HimanshuYadav <2019kuec2009@iiitkota.ac.in>
2020-06-12 12:08:39 +0530

    Updates

---
+++ precache-manifest.846911646c4cf238093c16ada2c45ad4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81eee8f68ee99279c44b69cd2915cb1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4fad1bae90d7ab537ba",
+    "url": "/static/css/main.a3745e42.chunk.css"
+  },
+  {
+    "revision": "915f91e1d9445f74004b",
+    "url": "/static/js/2.4ed12d36.chunk.js"
+  },
+  {
+    "revision": "6d98c309ab0e4d5aa5d30f35f2390953",
+    "url": "/static/js/2.4ed12d36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4fad1bae90d7ab537ba",
+    "url": "/static/js/main.37f36ab3.chunk.js"
+  },
+  {
+    "revision": "ca7c4156f1e30e666d9e",
+    "url": "/static/js/runtime-main.f3be89ca.js"
+  },
+  {
+    "revision": "313fe18b6b9b93d9129270804f255dee",
+    "url": "/static/media/about_coronavirus.313fe18b.png"
+  },
+  {
+    "revision": "c6842476bd9046768fd18db70510b2f9",
+    "url": "/static/media/brushed_alum.c6842476.png"
+  },
+  {
+    "revision": "1b62e7697fac44bb9fb590b64808f35f",
+    "url": "/static/media/corona_guard.1b62e769.png"
+  },
+  {
+    "revision": "7077f0d9b484228370260e978415541a",
+    "url": "/static/media/coronavirus.7077f0d9.png"
+  },
+  {
+    "revision": "bae30096701c56a015ac7e21bda16afa",
+    "url": "/static/media/coronavirus_white.bae30096.png"
+  },
+  {
+    "revision": "c5ecdc0ccc7b4dd6dd95be3006e7714c",
+    "url": "/static/media/symptoms.c5ecdc0c.png"
+  }
+]);
